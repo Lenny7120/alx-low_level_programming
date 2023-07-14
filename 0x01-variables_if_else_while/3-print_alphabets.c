1216ad85858fccafd7e740d369e42a8fb8bf9329
Lenny7120 <ocrank20@gmail.com>
2023-07-14 11:20:51 -0700

    Alphabets 2

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(d);
+			d++;
+		}
+	putchar('\n');
+	return (0)
+}
