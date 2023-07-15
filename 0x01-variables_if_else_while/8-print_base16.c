2c3326eded6e8f71abf00737be70b36530c41046
Lenny7120 <ocrank20@gmail.com>
2023-07-14 22:55:12 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	for(i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for(i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
