ca74dbecb46622ed4b035f816d3060f535936480
Lenny7120 <ocrank20@gmail.com>
2023-07-18 09:17:48 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,40 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - A program that prints _putchar
  * Return: 0 (success)
  */
 
-int _putchar(char c);
-void printWord(const char* word);
-void printNewLine();
-
 int main(void)
 {
-	printWord("_putchar");
-	printNewLine();
-
-	return 0;
-
-}
-
-int _putchar(char c)
-{
-	write(1, &c, 1);
-
-	return 0;
-}
-
-void printWord(const char* word)
-{
-	for (int i = 0; word [i] != '\0'; i++)
-{
-	_putchar(word[i]);
-}
-}
-
-void printNewLine()
-{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+        _putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
-
     
