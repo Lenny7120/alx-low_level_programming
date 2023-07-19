1570540629f945f2c4b93f9ebd923a004aaeda29
Lenny7120 <ocrank20@gmail.com>
2023-07-19 08:44:08 -0700

    98 battery street

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,34 +1,16 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- * followed by a new line
- * @n: print from this number
- * Return:...
+ * main - check the code
+ *
+ * Return: Always 0.
  */
-
-void print_to_98(int n)
+int main(void)
 {
-	int i, j;
-	if(n <= 98)
-	{
-		for(i = n; i <= 98; i++)
-		{
-			if(i != 98)
-				printf("%d,", i);
-			else if(i == 98)
-				printf("%d\n", i);
-		}
-	}	
-	        else if(n >= 98)
-		{
-			for(j = n; j >= 98; j--)
-			{
-				if(j != 98)
-					printf("%d", j);
-				else if(j == 98)
-					printf("%d\n", j);
-			}
-		}
+    print_to_98(0);
+    print_to_98(98);
+    print_to_98(111);
+    print_to_98(81);
+    print_to_98(-10);
+    return (0);
 }
