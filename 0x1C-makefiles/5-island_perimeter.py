e749ac4f598eb941ab5a6a53abb2a8e5f324d450
Lenny7120 <ocrank20@gmail.com>
2023-12-20 09:05:41 -0800

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,9 +2,10 @@
 """Module that calculates the perimeter of an island in a grid."""
 
 
-def num_water_neighbors(grid, i, j):            """Returns the number of water neighbors a cell has in a grid."""
+def num_water_neighbors(grid, i, j):    
+    """Returns the number of water neighbors a cell has in a grid."""
      
-     num = 0
+    num = 0
 
     if i <= 0 or not grid[i - 1][j]:
         num += 1
